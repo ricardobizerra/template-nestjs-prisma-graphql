675d8136830d8eca4e3213cc01577b955438f6e2
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-12-01 00:25:52 -0300

    feat: config redis env and docker

--- src/env.ts
+++ src/env.ts
@@ -4,14 +4,22 @@ const ambients = ['development', 'staging', 'production'] as const;
 
 export const envSchema = z.object({
   APP_NAME: z.coerce.string().optional().default('rblf'),
+
   PORT: z.coerce.number().optional().default(3333),
   NODE_ENV: z.enum(ambients).default('development'),
+
   POSTGRES_USER: z.coerce.string(),
   POSTGRES_PASSWORD: z.coerce.string(),
   POSTGRES_DB: z.coerce.string(),
   POSTGRES_HOST: z.coerce.string(),
   POSTGRES_PORT: z.coerce.number().optional().default(5432),
   DATABASE_URL: z.coerce.string(),
+
+  REDIS_PORT: z.coerce.number().optional().default(6379),
+  REDIS_HOST: z.coerce.string(),
+  REDIS_PASSWORD: z.coerce.string(),
+  REDIS_DB: z.coerce.number().optional().default(0),
+  REDIS_URL: z.coerce.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
