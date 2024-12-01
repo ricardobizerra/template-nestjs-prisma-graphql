8dbe4d743d2e31b53fbf7df6826f5e1f07dabf9c
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-12-01 00:26:33 -0300

    feat: user added subscription

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,12 +1,24 @@
-import { Args, ID, Info, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  ID,
+  Info,
+  Mutation,
+  Query,
+  Resolver,
+  Subscription,
+} from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { GraphQLResolveInfo } from 'graphql';
 import { getQueriedFields } from 'src/utils/get-queried-fields';
 import { UserCreateInput, User } from 'src/graphql/prisma-client';
+import { RedisService } from 'src/redis/redis.service';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly redisService: RedisService,
+  ) {}
 
   @Query(() => [User], { name: 'users' })
   async findMany(@Info() info: GraphQLResolveInfo) {
@@ -21,6 +33,14 @@ export class UserResolver {
 
   @Mutation(() => User, { name: 'createUser' })
   async create(@Args('data') data: UserCreateInput) {
+    this.redisService.publish('userAdded', { userAdded: data });
     return this.userService.create(data);
+  }
+
+  @Subscription(() => User, {
+    name: 'userAdded',
+  })
+  subscribeToUserAdded() {
+    return this.redisService.asyncIterator('userAdded');
   }
 }
