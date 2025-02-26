eefdcc6a79b2bd52c39d6ad65e65ef95974bab27
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-02-25 23:28:39 -0300

    feat: update select object to handle differences between database and graphql model types

--- src/utils/select-object.ts
+++ src/utils/select-object.ts
@@ -1,8 +1,70 @@
-export function selectObject<T>(
-  queriedFields: string[],
-): Partial<Record<keyof T, true>> {
-  return queriedFields.reduce(
-    (acc, field) => ({ ...acc, [field]: true }),
-    {} as Partial<Record<keyof T, true>>,
+type ExtraModelFields<TDatabase, TModel> = Exclude<
+  keyof TModel,
+  keyof TDatabase
+>;
+
+type RequiredHashFields<TDatabase, TModel> =
+  ExtraModelFields<TDatabase, TModel> extends never
+    ? Record<string, never> & {
+        DEFAULT: (keyof TDatabase)[];
+      }
+    : {
+        [K in ExtraModelFields<TDatabase, TModel>]: (keyof TDatabase)[];
+      } & {
+        DEFAULT?: (keyof TDatabase)[];
+      };
+
+type SelectObjectParams<TDatabase, TModel> =
+  ExtraModelFields<TDatabase, TModel> extends never
+    ? [
+        queriedFields: (keyof TModel)[],
+        hashDifferentFields?: RequiredHashFields<TDatabase, TModel>,
+      ]
+    : [
+        queriedFields: (keyof TModel)[],
+        hashDifferentFields: RequiredHashFields<TDatabase, TModel>,
+      ];
+
+type SelectObjectReturn<TDatabase> = Partial<Record<keyof TDatabase, true>>;
+
+export function selectObject<
+  TDatabase extends Record<string, any>,
+  TModel extends Partial<TDatabase>,
+>(
+  ...args: SelectObjectParams<TDatabase, TModel>
+): SelectObjectReturn<TDatabase> {
+  const [queriedFields, hashDifferentFields] = args as SelectObjectParams<
+    TDatabase,
+    TModel
+  >;
+
+  const processFields = (fields: (keyof TDatabase)[]) =>
+    selectObject<TDatabase, TDatabase>(fields);
+
+  const reduceFields = (
+    acc: SelectObjectReturn<TDatabase>,
+    field: keyof TModel,
+  ) => {
+    if (hashDifferentFields && field in hashDifferentFields) {
+      const hashedFields = hashDifferentFields[
+        field as keyof RequiredHashFields<TDatabase, TModel>
+      ] as (keyof TDatabase)[];
+      return { ...acc, ...processFields(hashedFields) };
+    }
+    return { ...acc, [field]: true };
+  };
+
+  const reducedFields = queriedFields.reduce(
+    reduceFields,
+    {} as SelectObjectReturn<TDatabase>,
   );
+
+  if (!hashDifferentFields?.DEFAULT) {
+    return reducedFields;
+  }
+
+  const defaultFields = processFields(
+    hashDifferentFields.DEFAULT as (keyof TDatabase)[],
+  );
+  return { ...reducedFields, ...defaultFields };
 }
