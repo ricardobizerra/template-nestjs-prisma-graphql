60e4ee3f2f7cdb57a80aa565f3b1dce4713cbcc0
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-05-13 11:45:24 -0300

    feat: add cache module and improve config module on base test model

--- src/utils/create-test-model.ts
+++ src/utils/create-test-model.ts
@@ -5,6 +5,10 @@ import { RedisSubscriptionService } from '@/lib/redis/redis-subscription.service
 import { ModuleMetadata } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { RedisCacheService } from '@/lib/redis/redis-cache.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Env, envSchema } from '@/env';
+import { redisStore } from 'cache-manager-redis-store';
 
 export const createTestModel = async (
   metadata: ModuleMetadata,
@@ -15,10 +19,24 @@ export const createTestModel = async (
       ...(!!metadata.imports ? metadata.imports : []),
       RedisModule,
       PrismaModule,
-      ConfigModule,
+      ConfigModule.forRoot({
+        validate: (config) => envSchema.parse(config),
+        isGlobal: true,
+      }),
+      CacheModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService<Env, true>) => ({
+          store: await redisStore({
+            url: configService.get('REDIS_URL'),
+          }),
+        }),
+        isGlobal: true,
+      }),
     ],
     providers: [
       ...(!!metadata.providers ? metadata.providers : []),
+      RedisCacheService,
       RedisSubscriptionService,
       PrismaService,
       ConfigService,
