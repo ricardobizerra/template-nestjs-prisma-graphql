e762f112a3358967a2d402776e6920ff2bdae946
ricardobizerra <ricardo.bizerra19@gmail.com>
2026-01-22 23:47:27 -0300

    fix: app e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,19 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import request from 'supertest';
 import { AppModule } from '@/app.module';
-import { beforeEach, it, describe } from 'vitest';
+import { beforeEach, it, describe, afterAll } from 'vitest';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
